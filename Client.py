05be19ef9ddc135162a06a66b8f99371c7a6ee79
amir <amirsabbagh2000z@gmail.com>
2021-07-07 20:43:30 +0430

    "feat : Add test udp server and client files"

--- Client.py
+++ Client.py
@@ -3,38 +3,38 @@ import socket, random
 
 class Client:
     def __init__(self):
-        self.__serverIP = "127.0.0.1"
-        self.__port = 8080
-        self.__timeoutTimer = 8080
-        self.__discoverTimer = 8080
+        self.__serverIP, self.__port = "127.0.0.1", 8080
+        self.__timeoutTimer = 80
+        self.__discoverTimer = 80
         self.__backoff_cutoff = 120
         self.__initial_interval = 10
         self.__previousInterval = 0
 
     def runClient(self):
-        client = socket.socket(family=socket.AF_INET,type= socket.SOCK_DGRAM)
-        client.sendto()
-
+        clientSock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
         while True:
             if True:
                 print("Client : sending DISCOVERY ...")
-                client.sendto(self.DHCP_DISCOVERY_Sender(),(self.__serverIP, self.__port))
-                # client.sendall(self.DHCP_DISCOVERY_Sender())
+                clientSock.sendto(self.DHCP_DISCOVERY_Sender(), (self.__serverIP, self.__port))
+
                 print("Client : receiving OFFER from server ...")
-                DHCP_Offer = client.recvfrom(2048)
+                DHCP_Offer = clientSock.recv(2048)
                 print("Server OFFER :", DHCP_Offer.decode(), end="\n")
+
                 print("Client : sending REQUEST ...")
-                client.sendto(self.DHCP_REQUEST_Sender(),(self.__serverIP, self.__port))
+                clientSock.sendto(self.DHCP_REQUEST_Sender(), (self.__serverIP, self.__port))
+
                 print("Client : receiving ACK from server ...")
-                DHCP_Ack = client.recvfrom(2048)
+                DHCP_Ack = clientSock.recv(2048)
                 print("\u2592\u2592\u2592\u2592\u2592")
                 print("Server ACK :", DHCP_Ack.decode(), end="\n")
+
                 if DHCP_Ack == 'bye':
                     break
             else:
                 pass
-        client.close()
+        clientSock.close()
 
     def DHCP_REQUEST_Sender(self):
         return ""
